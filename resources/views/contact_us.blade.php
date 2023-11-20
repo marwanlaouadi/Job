1da5026d806a49ecb124c695c0f8acb59364b6b2
marwanlaouadi <marwanlaouadi@gmail.com>
2023-11-20 21:50:22 +0100

    pitchou

--- resources/views/contact_us.blade.php
+++ resources/views/contact_us.blade.php
@@ -1,106 +1,76 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contactez-nous</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #f0f0f0;
-            margin: 0;
-            padding: 0;
-        }
+  <meta charset="UTF-8">
+  <title>Contact Us</title>
+  <style>
+    body {
+      margin: 0;
+      padding: 0;
+      font-family: Arial, sans-serif;
+      background: linear-gradient(to bottom right, #0072ff, #00c6ff);
+      /* Adjust the gradient colors and direction as needed */
+      color: white;
+    }
 
-        header {
-            background-color: #333;
-            color: white;
-            text-align: center;
-            padding: 20px 0;
-        }
+    .contact-section {
+      padding: 50px 20px;
+      text-align: center;
+    }
 
-        main {
-            margin: 20px;
-        }
+    .contact-form {
+      max-width: 400px;
+      margin: 0 auto;
+      padding: 20px;
+      border-radius: 8px;
+      box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
+      background-color: rgba(255, 255, 255, 0.1);
+      /* Adjust opacity as needed */
+    }
 
-        section {
-            background-color: white;
-            padding: 20px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-            border-radius: 5px;
-        }
+    .contact-form input[type="text"],
+    .contact-form input[type="email"],
+    .contact-form textarea {
+      width: 100%;
+      padding: 10px;
+      margin-bottom: 15px;
+      border: 1px solid #ddd;
+      border-radius: 5px;
+      box-sizing: border-box;
+      background-color: rgba(255, 255, 255, 0.8);
+      /* Adjust input background opacity as needed */
+    }
 
-        h1, h2 {
-            font-size: 24px;
-        }
+    .contact-form textarea {
+      height: 100px;
+    }
 
-        label {
-            display: block;
-            margin-top: 10px;
-        }
+    .contact-form input[type="submit"] {
+      width: 100%;
+      padding: 10px;
+      border: none;
+      border-radius: 5px;
+      background-color: #00aaff;
+      color: white;
+      cursor: pointer;
+      transition: background-color 0.3s ease;
+    }
 
-        input[type="text"],
-        input[type="email"],
-        textarea {
-            width: 100%;
-            padding: 10px;
-            margin-top: 5px;
-            border: 1px solid #ccc;
-            border-radius: 5px;
-        }
-
-        input[type="submit"] {
-            background-color: #333;
-            color: white;
-            padding: 10px 20px;
-            border: none;
-            border-radius: 5px;
-            cursor: pointer;
-        }
-
-        input[type="submit"]:hover {
-            background-color: #555;
-        }
-
-        footer {
-            background-color: #333;
-            color: white;
-            text-align: center;
-            padding: 10px 0;
-        }
-
-        a {
-            color: white;
-            text-decoration: none;
-        }
-    </style>
+    .contact-form input[type="submit"]:hover {
+      background-color: #0077cc;
+    }
+  </style>
 </head>
 <body>
-    <header>
-        <h1>Contactez-nous</h1>
-    </header>
-
-    <main>
-        <section>
-            <h2>Nous Contacter</h2>
-            <p>Vous pouvez nous contacter en utilisant le formulaire ci-dessous :</p>
-            <form action="traiter_formulaire.php" method="post">
-                <label for="nom">Nom :</label>
-                <input type="text" id="nom" name="nom" required>
-
-                <label for="email">Adresse Email :</label>
-                <input type="email" id="email" name="email" required>
-
-                <label for="message">Message :</label>
-                <textarea id="message" name="message" rows="4" required></textarea>
-
-                <input type="submit" value="Envoyer">
-            </form>
-        </section>
-    </main>
-
-    <footer>
-        <p>Contactez-nous à l'adresse email : <a href="mailto:contact@exemple.com">contact@exemple.com</a></p>
-    </footer>
+  <section class="contact-section">
+    <h1>Contact Us</h1>
+    <form class="contact-form" action="#" method="post">
+      <input type="text" name="name" placeholder="nom" required>
+      <input type="email" name="email" placeholder="email" required>
+      <textarea name="suject" placeholder=" subject" required></textarea>
+      <textarea name="message" placeholder=" message" required></textarea>
+      <input type="submit" value="envoyer">
+    </form>
+  </section>
 </body>
 </html>
